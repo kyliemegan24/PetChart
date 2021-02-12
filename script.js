a379b1f93371eeb51bd29d00365adfb75f5d9cd8
Kylie S <kyliemegan24@gmail.com>
2021-02-12 10:05:24 -0600

    pushing first set of files

---
+++ script.js
@@ -0,0 +1,34 @@
+
+//Function to validate email on Signup page
+
+function ValidateEmail(inputText)
+{
+var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
+}
+}
+
+//Function to validate phone number on FAQs page
+
+function validatePhone(){
+    var phoneNumber = document.getElementById('phone-number').value;
+    var phoneRGEX = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+    var phoneResult = phoneRGEX.test(phoneNumber);
+    if(phoneResult == false)
+    {
+      alert('Please enter a valid phone number');
+      return false;
+    }
+   return true;
+}
+
